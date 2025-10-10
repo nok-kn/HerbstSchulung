39202c39dd21e8356f12c844a84f2fa2b4b4fa53
Krzysztof Nowak <kn@nowak-it.de>
2025-10-10 15:10:53 +0200

    Add lock example

---
+++ HerbstSchulung.CSharpFeatures/LockExample.cs
@@ -0,0 +1,19 @@
+﻿
+namespace HerbstSchulung.CSharpFeatures
+{
+    public class LockExample
+    {
+        // funktioniert NICHT mit.NET 8
+        //  System.Threading.Lock myLock = new();  //  existiert nicht
+
+        private readonly object  _myLock = new object();
+
+        private void DoSomething()
+        {
+            lock (_myLock) 
+            {
+                // ...
+            }
+        }
+    }
+}
